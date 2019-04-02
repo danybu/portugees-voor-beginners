2c7c7c0a9cea9572820c3bb42fd8e918252fc96f
Dany Buts <danybu.danybu@gmail.com>
2019-04-02 18:23:42 +0200

    sidebar integration lookup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'words/index'
   get 'words/lookup'
+  get 'lookup', to: 'words#lookup'
   devise_for :users
-  root to: 'words#lookup'
+  root to: 'words#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
